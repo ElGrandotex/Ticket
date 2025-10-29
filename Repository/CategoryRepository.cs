89eb31313cfb09c5480b9e710325395187cad8b3
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-10-28 22:22:59 -0500

    Patron Repository Category

---
+++ Repository/CategoryRepository.cs
@@ -0,0 +1,59 @@
+﻿using Ticket.Data;
+using Ticket.Models;
+using Ticket.Repository.IRepository;
+
+namespace Ticket.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryRepository(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public bool CategoryExists(int categoyId)
+        {
+            return _db.Categories.Any(c => c.Id == categoyId);
+        }
+
+        public bool CategoryExists(string categoryName)
+        {
+            return _db.Categories.Any(c => c.Name.ToLower().Trim() == categoryName.ToLower().Trim());
+        }
+
+        public bool CreateCategory(Category category)
+        {
+            category.CreationDate = DateTime.Now;
+            _db.Categories.Add(category);
+            return Save();
+        }
+
+        public bool DeleteCategory(Category category)
+        {
+            _db.Categories.Remove(category);
+            return Save();
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _db.Categories.OrderBy(c => c.Name).ToList();
+        }
+
+        public Category GetCategory(int categoryId)
+        {
+            return _db.Categories.FirstOrDefault(c => c.Id == categoryId) ?? throw new InvalidOperationException("La categoria no existe");
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0;
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            category.CreationDate = DateTime.Now;
+            _db.Categories.Update(category);
+            return Save();
+        }
+    }
+}
---
+++ Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Ticket.Models;
+
+namespace Ticket.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int categoryId);
+        bool CategoryExists(int categoyId);
+        bool CategoryExists(string categoryName);
+        bool CreateCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool Save();
+
+    }
+}
