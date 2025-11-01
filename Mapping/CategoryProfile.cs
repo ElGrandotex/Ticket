153ef889b2af66402a3c8910c25e23d60d0fbf5f
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-10-31 21:34:38 -0500

    Dtos

---
+++ Mapping/CategoryProfile.cs
@@ -0,0 +1,28 @@
+﻿using Mapster;
+using Ticket.Models;
+using Ticket.Models.Dtos;
+
+namespace Ticket.Mapping
+{
+    public class CategoryProfile : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Category, CategoryDto>()
+              .Map(dest => dest.Id, src => src.Id)
+              .Map(dest => dest.Name, src => src.Name)
+              .Map(dest => dest.CreationDate, src => src.CreationDate);
+
+            config.NewConfig<CategoryDto, Category>()
+              .Map(dest => dest.Id, src => src.Id)
+              .Map(dest => dest.Name, src => src.Name)
+              .Map(dest => dest.CreationDate, src => src.CreationDate);
+
+            config.NewConfig<Category, CreateCategoryDto>()
+                  .Map(dest => dest.Name, src => src.Name);
+
+            config.NewConfig<CreateCategoryDto, Category>()
+                    .Map(dest => dest.Name, src => src.Name);
+        }
+    }
+}
