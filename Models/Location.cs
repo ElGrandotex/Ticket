29f22f06c21ace93e9a985c70eaff416d7a7f6a2
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-11-03 19:23:44 -0500

    Create Table Location

---
+++ Models/Location.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticket.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Address { get; set; } = string.Empty;
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Capacity { get; set; }
+        [Required]
+        public string City { get; set; } = string.Empty;
+        [Required]
+        public string State { get; set; } = string.Empty;
+    }
+}
