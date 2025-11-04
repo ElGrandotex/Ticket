3194d97251590250fe7b6ab1a20c7316010b7033
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-11-03 23:39:13 -0500

    Location Dto's

---
+++ Models/Dtos/Location/CreateLocationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ticket.Models.Dtos.Location
+{
+    public class CreateLocationDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+    }
+}
