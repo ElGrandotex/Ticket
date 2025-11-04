dd4c57d20f3a561ed445dd8fe378caf6eb7bca0e
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-11-03 23:46:30 -0500

    Interface Location & Event Repository

---
+++ Repository/IRepository/IEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using Ticket.Models;
+
+namespace Ticket.Repository.IRepository
+{
+    public interface IEventRepository
+    {
+        ICollection<Event> GetEvents();
+        Event? GetEvent(int eventId);
+        bool EventExists(int eventId);
+        bool EventExists(string eventName);
+        Event? GetEventByName(string eventName);
+        bool BuyTickets(int eventId, int numberOfTickets);
+        bool CreateEvent(Event eventModel);
+        bool UpdateEvent(Event eventModel);
+        bool DeleteEvent(Event eventModel);
+        bool Save();
+    }
+}
---
+++ Repository/IRepository/ILocationRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ticket.Models;
+
+namespace Ticket.Repository.IRepository
+{
+    public interface ILocationRepository
+    {
+        ICollection<Location> GetLocations();
+        Location? GetLocation(int locationId);
+        bool LocationExists(int locationId);
+        bool LocationExists(string locationName);
+        bool CreateLocation(Location location);
+        bool UpdateLocation(Location location);
+        bool DeleteLocation(Location location);
+        bool Save();
+    }
+}
