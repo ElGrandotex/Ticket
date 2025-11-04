65bf52fccdab4bffb9b7a5305d84d88a12c6a60d
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-11-03 19:23:55 -0500

    Create Table Event

---
+++ Models/Event.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ticket.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        [Range(0, double.MaxValue)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public string ImgUrl { get; set; } = string.Empty;
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? LastUpdatedAt { get; set; } = null;
+
+        // Foreign Key
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public required Category Category { get; set; }
+
+        // Foreign Key
+        public int LocationId { get; set; }
+        [ForeignKey("LocationId")]
+        public required Location Location { get; set; }
+    }
+}
