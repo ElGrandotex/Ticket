551ff3fcda2c602c59a86942ecb84b24015087bf
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-11-03 23:38:41 -0500

    Event Dto's

---
+++ Models/Dtos/Event/EventDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ticket.Models.Dtos.Event
+{
+    public class EventDto
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string ImgUrl { get; set; } = string.Empty;
+        public int Stock { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? LastUpdatedAt { get; set; } = null;
+
+        // Foreign Key
+        public int CategoryId { get; set; }
+
+        // Foreign Key
+        public int LocationId { get; set; }
+    }
+}
