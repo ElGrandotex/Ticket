c3bb2d97dbcf66ff09f166429cdfef51a67e63c5
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-11-03 23:39:34 -0500

    Event & Location Profile

---
+++ Mapping/EventProfile.cs
@@ -0,0 +1,79 @@
+﻿using Mapster;
+using Ticket.Models;
+using Ticket.Models.Dtos.Event;
+
+namespace Ticket.Mapping
+{
+    public class EventProfile : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Event, EventDto>()
+              .Map(dest => dest.EventId, src => src.EventId)
+              .Map(dest => dest.Name, src => src.Name)
+              .Map(dest => dest.Date, src => src.Date)
+              .Map(dest => dest.Time, src => src.Time)
+              .Map(dest => dest.Description, src => src.Description)
+              .Map(dest => dest.Price, src => src.Price)
+              .Map(dest => dest.ImgUrl, src => src.ImgUrl)
+              .Map(dest => dest.Stock, src => src.Stock)
+              .Map(dest => dest.CreatedAt, src => src.CreatedAt)
+              .Map(dest => dest.LastUpdatedAt, src => src.LastUpdatedAt)
+              .Map(dest => dest.CategoryId, src => src.CategoryId)
+              .Map(dest => dest.LocationId, src => src.LocationId);
+            config.NewConfig<EventDto, Event>()
+                .Map(dest => dest.EventId, src => src.EventId)
+                .Map(dest => dest.Name, src => src.Name)
+                .Map(dest => dest.Date, src => src.Date)
+                .Map(dest => dest.Time, src => src.Time)
+                .Map(dest => dest.Description, src => src.Description)
+                .Map(dest => dest.Price, src => src.Price)
+                .Map(dest => dest.ImgUrl, src => src.ImgUrl)
+                .Map(dest => dest.Stock, src => src.Stock)
+                .Map(dest => dest.CreatedAt, src => src.CreatedAt)
+                .Map(dest => dest.LastUpdatedAt, src => src.LastUpdatedAt)
+                .Map(dest => dest.CategoryId, src => src.CategoryId)
+                .Map(dest => dest.LocationId, src => src.LocationId);
+            config.NewConfig<Event, CreateEventDto>()
+                .Map(dest => dest.Name, src => src.Name)
+                .Map(dest => dest.Date, src => src.Date)
+                .Map(dest => dest.Time, src => src.Time)
+                .Map(dest => dest.Description, src => src.Description)
+                .Map(dest => dest.Price, src => src.Price)
+                .Map(dest => dest.ImgUrl, src => src.ImgUrl)
+                .Map(dest => dest.Stock, src => src.Stock)
+                .Map(dest => dest.CategoryId, src => src.CategoryId)
+                .Map(dest => dest.LocationId, src => src.LocationId);
+            config.NewConfig<CreateEventDto, Event>()
+                .Map(dest => dest.Name, src => src.Name)
+                .Map(dest => dest.Date, src => src.Date)
+                .Map(dest => dest.Time, src => src.Time)
+                .Map(dest => dest.Description, src => src.Description)
+                .Map(dest => dest.Price, src => src.Price)
+                .Map(dest => dest.ImgUrl, src => src.ImgUrl)
+                .Map(dest => dest.Stock, src => src.Stock)
+                .Map(dest => dest.CategoryId, src => src.CategoryId)
+                .Map(dest => dest.LocationId, src => src.LocationId);
+            config.NewConfig<Event, UpdateEventDto>()
+                .Map(dest => dest.Name, src => src.Name)
+                .Map(dest => dest.Date, src => src.Date)
+                .Map(dest => dest.Time, src => src.Time)
+                .Map(dest => dest.Description, src => src.Description)
+                .Map(dest => dest.Price, src => src.Price)
+                .Map(dest => dest.ImgUrl, src => src.ImgUrl)
+                .Map(dest => dest.Stock, src => src.Stock)
+                .Map(dest => dest.CategoryId, src => src.CategoryId)
+                .Map(dest => dest.LocationId, src => src.LocationId);
+            config.NewConfig<UpdateEventDto, Event>()
+                .Map(dest => dest.Name, src => src.Name)
+                .Map(dest => dest.Date, src => src.Date)
+                .Map(dest => dest.Time, src => src.Time)
+                .Map(dest => dest.Description, src => src.Description)
+                .Map(dest => dest.Price, src => src.Price)
+                .Map(dest => dest.ImgUrl, src => src.ImgUrl)
+                .Map(dest => dest.Stock, src => src.Stock)
+                .Map(dest => dest.CategoryId, src => src.CategoryId)
+                .Map(dest => dest.LocationId, src => src.LocationId);
+        }
+    }
+}
---
+++ Mapping/LocationProfile.cs
@@ -0,0 +1,39 @@
+﻿using Mapster;
+using Ticket.Models;
+using Ticket.Models.Dtos.Location;
+
+namespace Ticket.Mapping
+{
+    public class LocationProfile : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Location, LocationDto>()
+              .Map(dest => dest.LocationId, src => src.LocationId)
+              .Map(dest => dest.Name, src => src.Name)
+              .Map(dest => dest.Address, src => src.Address)
+              .Map(dest => dest.Capacity, src => src.Capacity)
+              .Map(dest => dest.City, src => src.City)
+              .Map(dest => dest.State, src => src.State);
+            config.NewConfig<LocationDto, Location>()
+              .Map(dest => dest.LocationId, src => src.LocationId)
+              .Map(dest => dest.Name, src => src.Name)
+              .Map(dest => dest.Address, src => src.Address)
+              .Map(dest => dest.Capacity, src => src.Capacity)
+              .Map(dest => dest.City, src => src.City)
+              .Map(dest => dest.State, src => src.State);
+            config.NewConfig<Location, CreateLocationDto>()
+              .Map(dest => dest.Name, src => src.Name)
+              .Map(dest => dest.Address, src => src.Address)
+              .Map(dest => dest.Capacity, src => src.Capacity)
+              .Map(dest => dest.City, src => src.City)
+              .Map(dest => dest.State, src => src.State);
+            config.NewConfig<CreateLocationDto, Location>()
+              .Map(dest => dest.Name, src => src.Name)
+              .Map(dest => dest.Address, src => src.Address)
+              .Map(dest => dest.Capacity, src => src.Capacity)
+              .Map(dest => dest.City, src => src.City)
+              .Map(dest => dest.State, src => src.State);
+        }
+    }
+}
